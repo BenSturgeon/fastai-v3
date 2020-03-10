6ccbbedf30403ed2d628ca939b159dafb8239a8c
Benjamin Sturgeon <sturgeonkid@gmail.com>
2020-03-10 16:32:39 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = https://drive.google.com/uc?export=download&id=1nLGLt56GqDsEw8FNGpIUecKohBgTWDdN
+export_file_url = 'https://drive.google.com/uc?export=download&id=1nLGLt56GqDsEw8FNGpIUecKohBgTWDdN'
 export_file_name = 'export.pkl'
 
 classes = ['Christ', 'Buddha']
